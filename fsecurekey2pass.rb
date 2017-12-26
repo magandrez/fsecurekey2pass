d282e94096021337adbc1f1f96e4278bdb947793
Manuel Gonzalez <manuel.gonzalez@foodie.fm>
2017-12-26 17:35:25 +0200

    Various fixes
    - Fixed license.
    -Fixed options parsing.
    -Uses Open3.popen3 instead of IO.popen.
    -Fixes data massaging.
    -Fixes error reporting.

--- fsecurekey2pass.rb
+++ fsecurekey2pass.rb
@@ -1,44 +1,25 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-
-# Copyright (C) 2017 Manuel González <manuel@digitalfoodie.fm>.
+#
+# Copyright (C) 2017 Manuel González <manuel@foodie.fm>.
 # All Rights Reserved. This file is licensed under GPLv2+.
 #
-# F-Secure KEY Importer
+# F-Secure KEY importer to zx2c4's password-store (the standard Unix password manager)
 #
 # Reads files exported from F-Secure (.fsk) and imports them into pass.
-# The export file is a JSON formatted file like:
-#
-# {
-#  "data": {
-#     <GUID>: {
-#       "passwordList": <Array>,
-#       "style": <String>,
-#       "rev": <Integer>,
-#       "favorite": <Integer>,
-#       "url": <String>,
-#       "creditNumber": <String>,
-#       "type": <Integer>,
-#       "creditExpiry": <String>,
-#       "notes": <String>,
-#       "password": <String>, 
-#       "creditCvv": <Integer>,
-#       "service": <String>,
-#       "username": <String>,
-#       "color": <String>
-#     } 
-#   }
-# }
-#
-# Supports importing metadata, adding them with `pass insert --multiline`
+# The export file is a JSON formatted file. The script has been put together
+# to use only standard Ruby libraries (i.e.: no Gemfile, bundler, etc)
+# Supports importing metadata, via `pass insert --multiline`.
 
 require "optparse"
 require "logger"
 require "json"
+require "pry"
+require "open3"
 
 options = {}
 options[:force] = false
-options[:group] = 'personal'
+options[:group] = "personal"
 options[:notes] = true
 
 optparse = OptionParser.new do |opts|
@@ -46,62 +27,17 @@ optparse = OptionParser.new do |opts|
   opts.on_tail("-h", "--help", "Displays this screen") { puts opts; exit }
   opts.on("-f", "--force",
           "Overrides existing passwords") do
-    options.force = true
+    options[:force] = true
   end
   opts.on("-g", "--group [FOLDER]",
           "Places passwords into FOLDER") do |group|
-    options.group = group
-  end
-  opts.on("-n", "--[no-]notes",
-          "Imports notes (--multiline option in pass)") do |meta|
-    options.notes = notes
+    options[:group] = group
   end
   begin
     opts.parse!
   rescue OptionParser::InvalidOption
     puts optparse
     exit
-  end
-end
-
-LOGGER = Logger.new(STDOUT)
-LOGGER.level = Logger::INFO
-
-def run
-  filename = ARGV.pop # last parameter being the filename  
-  unless valid_file?(filename)
-    LOGGER.error "Missing filename to import. " \
-      "Read `#{__FILE__} --help` for more information "
-    exit 1
-  else
-    LOGGER.info "Started the import of file #{filename} into pass."
-  end
-
-  unless valid_json?(filename)
-    exit 1
-  end
-
-  blob = load_file(filename)
-  data = blob[:data].to_a
-
-  LOGGER.info "Read #{data.count} accounts."
-
-  # Save the passwords
-  data.each do |pass|
-    IO.popen("pass insert #{"-f " if options.force}-m \"#{pass[:username]}\" > /dev/null", "w") do |io|
-      io.puts pass[:password]
-      if options.notes
-        io.puts "LOGIN: #{pass[:username]}" unless pass[:username].to_s.empty?
-        io.puts "URL: #{pass[:url]}" unless pass[:url].to_s.empty?
-        io.puts pass[:notes] unless pass[:notes].to_s.empty?
-      end
-    end
-    if $? == 0
-      LOGGER.info "Imported #{pass[:service]}"
-    else
-      LOGGER.error "Failed to import #{pass[:name]}"
-      errors << pass
-    end
   end
 end
 
@@ -135,4 +71,51 @@ def valid_json?(filename)
     return false
 end
 
-run
+# Main
+start_time = Time.now
+LOGGER = Logger.new(STDOUT)
+LOGGER.level = Logger::INFO
+
+filename = ARGV.pop # last parameter being the filename
+# Validate data input
+unless valid_file?(filename)
+  LOGGER.error "Missing filename to import. " \
+    "Read `#{__FILE__} --help` for more information "
+  exit 1
+else
+  LOGGER.info "Started the import of file #{filename} into pass."
+end
+
+unless valid_json?(filename)
+  exit 1
+end
+
+# Massage data
+blob = load_file(filename)
+data = []
+blob[:data].each{|_key, value| data.push(value)}
+
+LOGGER.info "Read #{data.count} accounts. Starting import..."
+
+# Save the accounts
+data.each do |pass|
+  cmd = "pass insert -m "
+  cmd << "-f " if options[:force]
+  cmd << "'#{options[:group]}/#{pass[:service]}'\n"
+  Open3.popen3(cmd) do |i,_o,e,_t|
+    i.puts pass[:password]
+    i.puts "URL: #{pass[:url]}" unless pass[:url].empty?
+    i.puts "Username: #{pass[:username]}" unless pass[:username].empty?
+    i.puts "Notes: #{pass[:notes]}" unless pass[:notes].empty?
+    i.puts "Card number: #{pass[:creditNumber]}" unless pass[:creditNumber].to_s.empty?
+    i.puts "Expiry date: #{pass[:creditExpiry]}" unless pass[:creditExpiry].to_s.empty?
+    i.puts "CVV: #{pass[:creditCvv]}" unless pass[:creditCvv].to_s.empty?
+    i.close_write
+    error = e.read
+    LOGGER.error "An error occurred with account #{pass[:service]}: #{error}" unless error.empty?
+  end
+end
+
+end_time = Time.now
+LOGGER.info "Finished importing accounts into pass. "\
+  "Import took #{end_time - start_time} seconds."
